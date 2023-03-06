5bbc01840e6b7322cfaade6eee96b1ddcb8c24b2
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-06 17:21:02 +0200

    setting up the structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,44 @@
+"use client"
+import { useState } from "react";
+
 export default function Home() {
+  const [temp , setTmp] = useState(null);
+  const [minTemp , setMinTemp] = useState('');
+  const [maxTemp , setMaxTemp] =useState('');
+  const [error , setError] =useState(false);
+
   return (
     <main>
-
+      <div>
+        <h1>Weather App</h1>
+      </div>
+      <div>
+        <input 
+        type="text"
+        placeholder="city"
+         />
+        <button>
+          Search
+        </button>
+      </div>
+      <div>
+        <div>
+          <p>Temperature:</p>
+          <p>{temp} °C</p>
+        </div>
+        <div>
+          <p>Min Temperature:</p>
+          <p>{minTemp} °C</p>
+        </div>
+        <div>
+          <p>Max Temperature:</p>
+          <p>{maxTemp} °C</p>
+        </div>
+        {error && (
+        <div>
+          <p>Couldn't find weather results.</p>          
+        </div>)}
+      </div>
     </main>
   )
 }
