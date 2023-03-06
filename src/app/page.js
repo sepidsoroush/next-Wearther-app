d9a08b00d8dec0fbe3577d30d3775ad54bad0006
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-06 18:09:53 +0200

    Integratie the API

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,35 @@
 "use client"
 import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  // declare variables
+  const [city , setCity] = useState('');
   const [temp , setTmp] = useState(null);
   const [minTemp , setMinTemp] = useState('');
   const [maxTemp , setMaxTemp] =useState('');
   const [error , setError] =useState(false);
+
+  const getWeather = () =>{
+    const options = {
+      method: 'GET',
+      url: 'https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D',
+      params: {units: 'auto', lang: 'en'},
+      headers: {
+        'X-RapidAPI-Key': '67b0005738msh9b550b5382f8820p1c3047jsn0e767ce79729',
+        'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
+      }
+    };
+    
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+    })
+      .catch(function (error) {
+        console.error(error);
+    });
+  }
 
   return (
     <main>
