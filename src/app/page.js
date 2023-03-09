bd746299ed9b6ada2154bd04f1d3aae85a2e375d
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-09 12:24:36 +0200

    re-write code

--- src/app/page.js
+++ src/app/page.js
@@ -5,46 +5,32 @@ import axios from "axios";
 export default function Home() {
   // declare variables
   const [city , setCity] = useState('');
-  const [temp , setTmp] = useState(null);
+  const [temp , setTemp] = useState(null);
   const [minTemp , setMinTemp] = useState('');
   const [maxTemp , setMaxTemp] =useState('');
   const [error , setError] =useState(false);
 
-  const getWeather = () =>{
-    const options = {
-      method: 'GET',
-      url: 'https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D',
-      params: {units: 'auto', lang: 'en'},
-      headers: {
-        'X-RapidAPI-Key': '67b0005738msh9b550b5382f8820p1c3047jsn0e767ce79729',
-        'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
-      }
-    };
-    
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-    })
-      .catch(function (error) {
-        console.error(error);
-    });
-  }
+
 
   return (
     <main>
       <div>
         <h1>Weather App</h1>
       </div>
-      <div>
+      
+      {/* <div>
         <input 
         type="text"
         placeholder="city"
+        onChange={event => setCity(event.target.value)}
          />
-        <button>
+        <button
+        onClick={getLocation}
+        >
           Search
         </button>
-      </div>
+      </div> */}
+      {/* {temp && (
       <div>
         <div>
           <p>Temperature:</p>
@@ -57,12 +43,12 @@ export default function Home() {
         <div>
           <p>Max Temperature:</p>
           <p>{maxTemp} °C</p>
-        </div>
-        {error && (
+        </div>        
+      </div>)}
+      {error && (
         <div>
           <p>Couldn't find weather results.</p>          
-        </div>)}
-      </div>
+        </div>)} */}
     </main>
   )
 }
