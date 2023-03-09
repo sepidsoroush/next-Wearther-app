9811a0ee3370b63949f7084f48dac12ccd61e3b7
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-03-09 17:56:09 +0200

    add a function to catch latitude & lonitude based on name

--- src/app/page.js
+++ src/app/page.js
@@ -10,51 +10,67 @@ export default function Home() {
   const [maxTemp , setMaxTemp] =useState('');
   const [error , setError] =useState(false);
 
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var lat = position.coords.latitude;
-      var long = position.coords.longitude;
-      console.log('Your latitude is: '+lat+' and longitude is '+long);
-      getWeather(lat , long);
-    });
-  } else {
-    alert("Geolocation is not supported by this browser.");
+  const getLocation = ()=>{
+    const cities = {
+      method : 'GET',
+      url : `http://api.openweathermap.org/geo/1.0/direct` ,
+      params :{q : `${city}` , limit : 1 , appid : '6e65a6b80661f81d2e592ae68a18c37c'}
+    }
+    axios
+      .request(cities)
+      .then(function(response){
+        console.log(response.data);
+        
+        
+      })
+      .catch(function(error){
+        console.log(error);
+      })
   }
-  const getWeather =(lat , long)=>{     
-    const options = {
-      method: 'GET',
-      url: `https://dark-sky.p.rapidapi.com/${lat},${long}`,
-      params: {units: 'auto', lang: 'en'},
-      headers: {
-        'X-RapidAPI-Key': '67b0005738msh9b550b5382f8820p1c3047jsn0e767ce79729',
-        'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
-      }
-    };
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
-  }
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(function (position) {
+  //     var lat = position.coords.latitude;
+  //     var long = position.coords.longitude;
+  //     console.log('Your latitude is: '+lat+' and longitude is '+long);
+  //     getWeather(lat , long);
+  //   });
+  // } else {
+  //   alert("Geolocation is not supported by this browser.");
+  // }
+  // const getWeather =(lat , long)=>{     
+  //   const options = {
+  //     method: 'GET',
+  //     url: `https://dark-sky.p.rapidapi.com/${lat},${long}`,
+  //     params: {units: 'auto', lang: 'en'},
+  //     headers: {
+  //       'X-RapidAPI-Key': '67b0005738msh9b550b5382f8820p1c3047jsn0e767ce79729',
+  //       'X-RapidAPI-Host': 'dark-sky.p.rapidapi.com'
+  //     }
+  //   };
+  //   axios.request(options).then(function (response) {
+  //     console.log(response.data);
+  //   }).catch(function (error) {
+  //     console.error(error);
+  //   });
+  // }
   return (
     <main>
       <div>
         <h1>Weather App</h1>
       </div>
-      <button
-        onClick={getWeather}
-        >
-          Search
-        </button>
-      {/* <div>
+      
+      <div>
         <input 
         type="text"
         placeholder="city"
         onChange={event => setCity(event.target.value)}
          />
-        
-      </div> */}
+        <button
+        onClick={getLocation}
+        >
+          Search
+      </button>
+      </div>
       {/* {temp && (
       <div>
         <div>
